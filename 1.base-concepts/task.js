f506aebe7b1fd93bef497589d5b37cd5a9508aad
Svetlana <svetlana.herissonn@gmail.com>
2025-02-26 20:41:22 +0300

    modified commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,18 +1,22 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let x1;
-  let x2;
+
 
   let d = b ** 2 - 4 * a * c;
 
     if(d > 0){
-        x1 = (-b + Math.sqrt(d)) / (2 * a);
-        x2 = (-b - Math.sqrt(d)) / (2 * a);
-        arr.push(x1, x2);
+      let x1;
+      let x2;
+
+      x1 = (-b + Math.sqrt(d)) / (2 * a);
+      x2 = (-b - Math.sqrt(d)) / (2 * a);
+      arr.push(x1, x2);
     } else if(d === 0) {
-        x1 = -b / (2 * a);
-        arr.push(x1);
+      let x1;
+
+      x1 = -b / (2 * a);
+      arr.push(x1);
     }     
   return arr;
 }
