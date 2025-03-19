5aaf523c0355940e7a96d64454aa4eb9c5bb73ea
Svetlana <svetlana.herissonn@gmail.com>
2025-03-19 10:11:57 +0300

    6.exception-closure

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿function parseCount(value) {
+	const parsedNumber = Number.parseFloat(value);
+	if (Number.isNaN(parsedNumber)) {
+		throw new Error("Невалидное значение");
+	} else {
+		return parsedNumber;
+	}
+}
+
+function validateCount(value) {
+	try {
+		return parseCount(value);
+	} catch (error) {
+		return error;
+	}
+}
+
+
+class Triangle {
+	constructor(a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		if (a + b <= c || a + c <= b || b + c <= a) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+	}
+
+	get perimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	get area() {
+		const p = 0.5 * (this.a + this.b + this.c);
+		const s = p * (p - this.a) * (p - this.b) * (p - this.c);
+
+		return Number(Math.sqrt(s).toFixed(3));
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c)
+	} catch (error) {
+		return {
+			perimeter: "Ошибка! Треугольник не существует",
+			area: "Ошибка! Треугольник не существует",
+		};
+	}
+}
